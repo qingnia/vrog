5a62833bcf8c5522742a00457694b383f22ad19e
chenshuai <1284530619@qq.com>
2023-02-11 11:47:48 +0800

    更新文档

---
+++ Assets/Script/MonsterManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterManager : MonoBehaviour
+{
+    public Object[] monsterTemplate;
+    public int monsterLimieNum = 0;
+    Dictionary<int, GameObject> monsters = new Dictionary<int, GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
