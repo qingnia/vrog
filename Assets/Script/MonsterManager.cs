e08b8aef5a304183df6e66ca08f6dd79d8ab2f53
陈帅 <chenshuai@sandboxol.com>
2023-02-13 11:34:59 +0800

    创建AI，暂解决不了动态room的nav

--- Assets/Script/MonsterManager.cs
+++ Assets/Script/MonsterManager.cs
@@ -6,16 +6,26 @@ public class MonsterManager : MonoBehaviour
 {
     public Object[] monsterTemplate;
     public int monsterLimieNum = 0;
-    Dictionary<int, GameObject> monsters = new Dictionary<int, GameObject>();
+    private Dictionary<int, GameObject> monsters = new Dictionary<int, GameObject>();
+    private int initNum = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (initNum < monsterLimieNum)
+        {
+            int index = Random.Range(0, monsterTemplate.Length);
+            GameObject go = Instantiate(monsterTemplate[index]) as GameObject;
+            go.transform.SetParent(this.gameObject.transform, false);
+            var pos = transform.position;
+            pos.x = pos.x + Random.Range(-9, 9);
+            pos.z = pos.z + Random.Range(-9,-9);
+            go.transform.position = pos;
+            initNum++;
+        }
     }
 }
